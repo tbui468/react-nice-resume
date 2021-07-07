79e6162540a0f7cfcf85bdacd891d2dd30db5bf7
Thomas <thomasthienbui@gmail.com>
2021-07-07 08:52:54 +0700

    Changed WORKS section to make it clear they are personal projects

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -65,9 +65,6 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
                 <a href={github} className="button btn github-btn">
                   <i className="fa fa-github"></i>Github
                 </a>
